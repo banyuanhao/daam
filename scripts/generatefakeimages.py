3a792cac22bf53ec45115d8352c912f0907024c9
banyh2000 <banyh2000@gmail.com>
2023-12-21 15:32:34 -0800

    sync

--- scripts/generatefakeimages.py
+++ scripts/generatefakeimages.py
@@ -1,9 +1,11 @@
 import argparse
 from daam import set_seed
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
 import torch
 import random
 import cv2
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '4'
 
     
 
@@ -17,17 +19,25 @@ args = parser.parse_args()
 
 model_id = 'stabilityai/stable-diffusion-2-base'
 device = 'cuda'
-pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
-pipe = pipe.to(device)
+
+# pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True).to(device)
+
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+).to(device)
+
+refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
+).to(device)
+
+
 prompt = args.prompt
 steps = args.steps
 negative_prompt = args.negative_prompt
 seed = args.seed if args.seed is not None else random.randint(1, 10000000)
 
 
-
-
 with torch.cuda.amp.autocast(dtype=torch.float16), torch.no_grad():
-    out = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=steps, generator=set_seed(seed))
+    out = pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=steps, generator=set_seed(seed))
     print(out.images[0])
     cv2.imwrite('pics/b.png',out.images[0])
