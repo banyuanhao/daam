f54b2c61cf6314841de67e96f625ef024f5b236d
banyuanhao <banyh2000@gmail.com>
2024-01-11 21:28:44 -0800

    revoke

--- models/diffuserpipeline.py
+++ models/diffuserpipeline.py
@@ -852,7 +852,7 @@ class StableDiffusionPipelineForNegativePrompts(DiffusionPipeline, TextualInvers
         do_classifier_free_guidance = guidance_scale > 1.0
 
         # 3. Encode input prompt
-        prompt_embeds = self._encode_prompt_total(
+        prompt_embeds = self._encode_prompt(
             prompt,
             device,
             num_images_per_prompt,
@@ -861,11 +861,19 @@ class StableDiffusionPipelineForNegativePrompts(DiffusionPipeline, TextualInvers
             prompt_embeds=prompt_embeds,
             negative_prompt_embeds=negative_prompt_embeds,
         )
-        
+        if negative_time is not None:
+            prompt_embeds_later = self._encode_prompt(
+                prompt,
+                device,
+                num_images_per_prompt,
+                do_classifier_free_guidance,
+            )
         # 4. Prepare timesteps
         self.scheduler.set_timesteps(num_inference_steps, device=device)
         timesteps = self.scheduler.timesteps
-
+        # modifying
+        #print(timesteps.shape)
+        #print(prompt_embeds.shape)
         # 5. Prepare latent variables
         num_channels_latents = self.unet.config.in_channels
         latents = self.prepare_latents(
@@ -887,7 +895,7 @@ class StableDiffusionPipelineForNegativePrompts(DiffusionPipeline, TextualInvers
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             for i, t in enumerate(timesteps):
                 # expand the latents if we are doing classifier free guidance
-                latent_model_input = torch.cat([latents] * 3) if do_classifier_free_guidance else latents
+                latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
                 #print(latent_model_input.shape)
                 # predict the noise residual
@@ -895,19 +903,14 @@ class StableDiffusionPipelineForNegativePrompts(DiffusionPipeline, TextualInvers
                 noise_pred = self.unet(
                     latent_model_input,
                     t,
-                    encoder_hidden_states = prompt_embeds,
+                    encoder_hidden_states = prompt_embeds if negative_time is None or i < negative_time else prompt_embeds_later,
                     cross_attention_kwargs=cross_attention_kwargs,
                 ).sample
                 
                 # perform guidance
                 if do_classifier_free_guidance:
-                    noise_pred_negative, noise_pred_positive, noise_pred_uncond = noise_pred.chunk(3)
-                    if negative_time is None:
-                        noise_pred = noise_pred_negative + guidance_scale * (noise_pred_positive - noise_pred_negative)
-                    elif i < negative_time:
-                        noise_pred = noise_pred_negative + guidance_scale * (noise_pred_positive - noise_pred_negative)
-                    else:
-                        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_positive - noise_pred_uncond)
+                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
