3991a81258b7fb003172dac3be7a9b0f3607bba4
banyuanhao <banyh2000@gmail.com>
2024-01-24 19:14:32 -0800

    sync

--- generating.md
+++ generating.md
@@ -1,2 +1,5 @@
 # Title
 *** sss ***
+tmux attach -t odfn_card_7
+
+python scripts/odfn/generating/generatefakeimages.py
--- scripts/odfn/generating/generatenoises.py
+++ scripts/odfn/generating/generatenoises.py
@@ -3,32 +3,35 @@
 
 import numpy as np
 from diffusers import StableDiffusionPipeline
-from utils_odfn import seeds_spilt, set_seed
+from utils_odfn import seeds_spilt, set_seed,seeds_plus_spilt
 from pathlib import Path
 
 spilt = 'val'
 version = 'version_2'
-seeds_sub = seeds_spilt(spilt)
 
-model_id = 'stabilityai/stable-diffusion-2-base'
-device = 'cuda'
-pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True).to(device)  
+for spilt in ['train', 'val', 'test']:
+    seeds_sub = seeds_plus_spilt(spilt)
+    model_id = 'stabilityai/stable-diffusion-2-base'
+    device = 'cuda'
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True).to(device)  
 
-dataset_path = Path(f'dataset/ODFN/{version}/{spilt}/noises')
+    dataset_path = Path(f'dataset/ODFN/{version}/{spilt}/noises')
 
-for seed in seeds_sub:
-    prompt = "chushu"
-    latent = pipe.get_latents(prompt=prompt, generator=set_seed(seed))
-    latent = latent[0].cpu().numpy().transpose(1,2,0)
-    # save latent as npy
-    np.save(dataset_path / f'{seed}.npy', latent)
-    
-    # LoadMultipleImagesFromFile
-    # for i in range(4):
-    #     img = latent[0][i].cpu().numpy()
-    #     print(np.max(img))
-    #     print(np.min(img))  
-    #     mmcv.imwrite(img, seed_path / f'{i}.png')
+    for seed in seeds_sub:
+        prompt = "chushu"
+        latent = pipe.get_latents(prompt=prompt, generator=set_seed(seed))
+        latent = latent[0].cpu().numpy().transpose(1,2,0)
+        # save latent as npy
+        if not dataset_path.exists():
+            dataset_path.mkdir(parents=True)
+        np.save(dataset_path / f'{seed}.npy', latent)
         
+        # LoadMultipleImagesFromFile
+        # for i in range(4):
+        #     img = latent[0][i].cpu().numpy()
+        #     print(np.max(img))
+        #     print(np.min(img))  
+        #     mmcv.imwrite(img, seed_path / f'{i}.png')
+            
+            
         
-    
