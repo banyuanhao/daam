ba6a2c8f5233e1c9f41ec19117c91adeab1a0556
Yuanhao Ban <banyh2000@valla.cs.ucla.edu>
2024-01-25 19:17:17 -0800

    sync

---
+++ scripts/odfn/generating/permuteannotations.py
@@ -0,0 +1,18 @@
+from utils_odfn import seeds_plus, seeds_plus_spilt, seeds_plus_shuffled_dict
+import random
+import numpy as np
+print(seeds_plus_shuffled_dict)
+# tmp = []
+
+# sub_seed = seeds_plus_spilt('train')
+# random.shuffle(sub_seed)
+# tmp.extend(sub_seed)
+# sub_seed = seeds_plus_spilt('val')
+# random.shuffle(sub_seed)
+# tmp.extend(sub_seed)
+# sub_seed = seeds_plus_spilt('test')
+# random.shuffle(sub_seed)
+# tmp.extend(sub_seed)
+
+
+
