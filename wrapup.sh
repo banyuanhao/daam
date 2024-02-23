d56e7572e71e992a22d777d4d315a620e5491ced
banyuanhao <banyh2000@gmail.com>
2024-02-23 14:14:18 -0800

    sync

--- wrapup.sh
+++ wrapup.sh
@@ -17,3 +17,4 @@
 # python scripts/ratio.py --prompt "A mountain range under a starry night sky" --negative_prompt "moon" --group wrapup_rem --seed 6463344 7056021 679216 4343903 8577767 8152514 6793668 5088744 7995971 6007072 --bound_box 20 0 30 10 --wandb
 
 # python scripts/ratio.py --prompt "A serene beach with crystal clear water at sunset" --negative_prompt "boats" --group wrapup_rem --seed 6463344 7056021 679216 4343903 8577767 8152514 6793668 5088744 7995971 6007072 --bound_box 20 0 30 10 --wandb
+
